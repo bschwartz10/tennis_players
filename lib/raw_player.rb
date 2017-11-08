8c40ec4f7c5a8a38241c5b44901250ea6536bc68
bschwartz10 <bschwartz92@gmail.com>
2017-11-08 14:34:48 -0500

    Revamp RawPlayer Class to use structs to reduce outside distruption
    initialized with the create player structs method
    this method creates a struct object based on the current instance
    formatting methods are now based on struct method calls instead of
    array indexes

--- lib/raw_player.rb
+++ lib/raw_player.rb
@@ -2,36 +2,53 @@ class RawPlayer
   attr_reader :raw_player
 
   def initialize(player_data)
-    @raw_player = player_data.gsub(/[,|]/, "").split(' ')
+    @raw_player = create_player_structs(player_data)
   end
 
   def normalize!
-    delete_middle_initial
-    swap_fav_color_and_dob
     format_date_of_birth
     format_gender
-    SanitizedPlayer.new(raw_player[0],raw_player[1], raw_player[2], raw_player[3], raw_player[4])
+    NormalizedPlayer.new(raw_player.last_name, raw_player.first_name, raw_player.gender, raw_player.date_of_birth, raw_player.favorite_color)
   end
 
 private
+  CommaPlayer = Struct.new(:last_name, :first_name, :gender, :favorite_color, :date_of_birth)
+  PipePlayer = Struct.new(:last_name, :first_name, :middle_initial, :gender, :favorite_color, :date_of_birth)
+  SpacePlayer = Struct.new(:last_name, :first_name, :middle_initial, :gender, :date_of_birth, :favorite_color)
+  NormalizedPlayer = Struct.new(:last_name, :first_name, :gender, :date_of_birth, :favorite_color)
 
-  SanitizedPlayer = Struct.new(:last_name, :first_name, :gender, :date_of_birth, :favorite_color)
-
-  def delete_middle_initial
-    raw_player.delete_at(2) if raw_player.count == 6
+  def create_player_structs(player_data)
+    remove_spaces_for_comma_and_pipe_players(player_data)
+    if player_data.include?(',')
+      normalized_data = player_data.split(',')
+      create_five_attribute_struct(normalized_data, CommaPlayer)
+    elsif player_data.include?('|')
+      normalized_data = player_data.split('|')
+      create_six_attribute_struct(normalized_data, PipePlayer)
+    else
+      normalized_data = player_data.split(' ')
+      create_six_attribute_struct(normalized_data, SpacePlayer)
+    end
   end
 
-  def swap_fav_color_and_dob
-    return unless raw_player[4].include?('-') || raw_player[4].include?('/')
-    raw_player[3], raw_player[4] = raw_player[4], raw_player[3]
+  def create_five_attribute_struct(normalized_data, player_type)
+    player_type.new(normalized_data[0], normalized_data[1], normalized_data[2], normalized_data[3], normalized_data[4])
+  end
+
+  def create_six_attribute_struct(normalized_data, player_type)
+    player_type.new(normalized_data[0], normalized_data[1], normalized_data[2], normalized_data[3], normalized_data[4], normalized_data[5])
+  end
+
+  def remove_spaces_for_comma_and_pipe_players(player_data)
+    player_data.tr!(' ', '') if player_data.include?(',') || player_data.include?('|')
   end
 
   def format_date_of_birth
-    raw_player[3].tr!('-', '/')
+    raw_player.date_of_birth.tr!('-', '/')
   end
 
   def format_gender
-    raw_player[2] = raw_player[2].start_with?('M') ? 'Male' : 'Female'
+    raw_player.gender = raw_player.gender.start_with?('M') ? 'Male' : 'Female'
   end
 
 end
