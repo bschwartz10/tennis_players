7f382413dad8c360e57efc60ff0316ba546480a4
bschwartz10 <bschwartz92@gmail.com>
2017-11-15 16:34:27 -0500

    Update to SortedPlayerCollection

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,9 +1,9 @@
 require './lib/player_io'
 require './lib/player'
-require './lib/player_collection'
+require './lib/sorted_player_collection'
 require 'date'
 
 files = ['./input/comma.txt', './input/pipe.txt', './input/space.txt']
 players = PlayerIo.import(files)
-sorted_players = PlayerCollection.new(players).sort_players
+sorted_players = SortedPlayerCollection.new(players)
 PlayerIo.export('./output/target_output.txt', sorted_players)
