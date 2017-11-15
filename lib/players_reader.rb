069f33cd188349250ef566c8c70eb679e40e476d
bschwartz10 <bschwartz92@gmail.com>
2017-11-15 09:50:53 -0500

    Instantiate updated Player class

--- lib/players_reader.rb
+++ lib/players_reader.rb
@@ -1,7 +1,8 @@
 class PlayersReader
   def self.read_players(file)
+    # constructor function
     raw_players = []
     IO.foreach(file) {|raw_player| raw_players << raw_player.strip}
-    raw_players.map {|raw_player| RawPlayer.new(raw_player)}
+    raw_players.map {|raw_player| Player.new(raw_player)}
   end
 end
