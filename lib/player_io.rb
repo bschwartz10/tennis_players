ab53fa876cf840718c968b6618bb48b09de98159
bschwartz10 <bschwartz92@gmail.com>
2017-11-15 15:11:42 -0500

    Combine PlayerReader and PlayerWriter into one class
    Holds import and export methods to take in data and write data

--- lib/player_io.rb
+++ lib/player_io.rb
@@ -1,19 +1,22 @@
 class PlayerIo
-  def self.import(file)
-    # constructor function
-    raw_players = []
-    IO.foreach(file) {|raw_player| raw_players << raw_player.strip}
-    raw_players.map {|raw_player| Player.new(raw_player)}
+  def self.import(files)
+    players = []
+    files.each do |file|
+      IO.foreach(file) {|raw_player| players << raw_player.strip}
+    end
+    players.map {|player| Player.new(player)}
   end
 
-  def self.export(player_batch)
-    File.open('./output/target_output.txt', 'a+') do |line|
-      line << "Output 1: \n"
-      player_batch.each do |player|
-        player.each {|attribute| line << "#{attribute} "}
+  def self.export(filename, player_collection)
+    File.open(filename, 'wb') do |line|
+      player_collection.each_with_index do |sorted_players, index|
+        line << "Output #{index + 1}: \n"
+        sorted_players.each do |player|
+          player.attributes.each {|attribute, value| line << "#{value} "}
+          line << "\n"
+        end
         line << "\n"
       end
-      line << "\n"
     end
   end
 
