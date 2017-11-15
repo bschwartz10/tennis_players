1e560574902b6114055a46720b322be09672b65e
bschwartz10 <bschwartz92@gmail.com>
2017-11-15 16:31:43 -0500

    Initializing with sorted players now

--- lib/player_io.rb
+++ lib/player_io.rb
@@ -1,4 +1,5 @@
 class PlayerIo
+
   def self.import(files)
     players = []
     files.each do |file|
@@ -9,7 +10,7 @@ class PlayerIo
 
   def self.export(filename, player_collection)
     File.open(filename, 'wb') do |line|
-      player_collection.each_with_index do |sorted_players, index|
+      player_collection.sorted_players.each_with_index do |sorted_players, index|
         line << "Output #{index + 1}: \n"
         sorted_players.each do |player|
           player.attributes.each {|attribute, value| line << "#{value} "}
