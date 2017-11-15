0760a1d35d3633e30726e3f897606a52c3486a1d
bschwartz10 <bschwartz92@gmail.com>
2017-11-15 16:33:22 -0500

    Correct order when sorting by last name

--- test/player_io_test.rb
+++ test/player_io_test.rb
@@ -6,7 +6,7 @@ class PlayerIoTest < Minitest::Test
   def setup
     files = ['./test/fixtures/comma_test_fixture.txt', './test/fixtures/pipe_test_fixture.txt', './test/fixtures/space_test_fixture.txt']
     @players = PlayerIo.import(files)
-    sorted_players = PlayerCollection.new(players).sort_players
+    sorted_players = SortedPlayerCollection.new(players)
     PlayerIo.export('./test/output/target_output.txt', sorted_players)
   end
 
@@ -58,15 +58,15 @@ class PlayerIoTest < Minitest::Test
     assert_equal 'Hingis Martina Female 4/2/1979 Green', output_file[19].strip
     assert_equal 'Smith Steve Male 3/3/1985 Red', output_file[20].strip
     assert_equal 'Output 3:', output_file[22].strip
-    assert_equal 'Abercrombie Neil Male 2/13/1943 Tan', output_file[23].strip
-    assert_equal 'Bishop Timothy Male 4/23/1967 Yellow', output_file[24].strip
-    assert_equal 'Bonk Radek Male 6/3/1975 Green', output_file[25].strip
-    assert_equal 'Bouillon Francis Male 6/3/1975 Blue', output_file[26].strip
+    assert_equal 'Smith Steve Male 3/3/1985 Red', output_file[23].strip
+    assert_equal 'Seles Monica Female 12/2/1973 Black', output_file[24].strip
+    assert_equal 'Kournikova Anna Female 6/3/1975 Red', output_file[25].strip
+    assert_equal 'Kelly Sue Female 7/12/1959 Pink', output_file[26].strip
     assert_equal 'Hingis Martina Female 4/2/1979 Green', output_file[27].strip
-    assert_equal 'Kelly Sue Female 7/12/1959 Pink', output_file[28].strip
-    assert_equal 'Kournikova Anna Female 6/3/1975 Red', output_file[29].strip
-    assert_equal 'Seles Monica Female 12/2/1973 Black', output_file[30].strip
-    assert_equal 'Smith Steve Male 3/3/1985 Red', output_file[31].strip
+    assert_equal 'Bouillon Francis Male 6/3/1975 Blue', output_file[28].strip
+    assert_equal 'Bonk Radek Male 6/3/1975 Green', output_file[29].strip
+    assert_equal 'Bishop Timothy Male 4/23/1967 Yellow', output_file[30].strip
+    assert_equal 'Abercrombie Neil Male 2/13/1943 Tan', output_file[31].strip
   end
 
 end
